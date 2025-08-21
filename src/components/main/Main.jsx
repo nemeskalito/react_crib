b09865528884fa47dcb642d3655603e47e5124f9
nemeskalito <borisenko.igor2021@yandex.ru>
2025-08-21 08:58:33 +0300

    spinner&loading

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,21 +3,41 @@ import { Route, Routes } from "react-router";
 import { allTheThemes } from "../book/allTheThemes";
 import { Navbar } from "./navbar/Navbar";
 import { Header } from "../header/Header";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export const Main = () => {
   const [video, setVideo] = useState("/fon1.mp4");
+  const [loading, setLoading] = useState(false);
+
+  const videoRef = useRef(null);
+
+  const changeVideo = (src) => {
+    if (src === video) return;
+    setLoading(true);
+    setVideo(src);
+  };
+
   return (
     <>
       <video
+        ref={videoRef}
         src={video}
         className="video-background"
         autoPlay
         muted
         loop
         playsInline
+        onCanPlayThrough={() => setLoading(false)}
       ></video>
-      <Header setVideo={setVideo} />
+      {loading && (
+        <div className="video-loader">
+          <div className="spinner-wrapper">
+            <div className="spinner"></div>
+          </div>
+        </div>
+      )}
+
+      <Header setVideo={changeVideo} />
       <div className="main">
         <Navbar />
         <div className="main__container">
